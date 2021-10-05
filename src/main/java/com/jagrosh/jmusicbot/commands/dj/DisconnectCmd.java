b46c3879f4b5ee01503d2a1f730a894ba7242eb5
jjjakey <superjjjakey@yahoo.com>
2021-10-05 01:01:00 -0500

    fixed author for new command

--- src/main/java/com/jagrosh/jmusicbot/commands/dj/DisconnectCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/dj/DisconnectCmd.java
@@ -1,5 +1,17 @@
 /*
- * jjjakey wrote this im sorry in advance
+ * Copyright 2021 John Grosh <john.a.grosh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.jagrosh.jmusicbot.commands.dj;
 
@@ -10,8 +22,9 @@ import com.jagrosh.jmusicbot.commands.DJCommand;
 
 /**
  *
- * @author jjjakey <mrjjjakey@gmail.com>
+ * @author Jacob J <mrjjjakey@gmail.com>
  */
+
 public class DisconnectCmd extends DJCommand 
 {
     public DisconnectCmd(Bot bot)
